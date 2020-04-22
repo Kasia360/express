0064ac98a0f395e9e63a771cb47b2ca0556d72b6
Kasia Brzezinska <kasiaplaskota4@gmail.com>
2020-04-22 23:18:27 +0200

    remove node modules

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
+
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
 
 app.use('/user', (req, res, next) => {
   res.send('You should log in to enter!');
@@ -18,31 +22,31 @@ app.use((req, res, next) => {
 app.use(express.static(path.join(__dirname + '/public')));
 
 app.get('/', (req, res) => {
-  res.show('index.html');
+  res.render('index');
 });
 
 app.get('/home', (req, res) => {
-  res.show('index.html');
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about');
 });
 
 app.get('/contact', (req, res) => {
-  res.show('contact.html');
+  res.render('contact');
 });
 
 app.get('/info', (req, res) => {
-  res.show('info.html');
+  res.render('info');
 });
 
 app.get('/history', (req, res) => {
-  res.show('history.html');
+  res.render('history');
 });
 
-app.get('/hello/:name', (req, res) => {
-  res.send('Hello' + req.params.id);
+app.get('/hello/:id', (req, res) => {
+  res.render('hello ', { name: req.params.id });
 });
 
 app.use((req, res) => {
